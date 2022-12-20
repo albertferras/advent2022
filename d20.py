35957763fb3e25fa3f0811701ef8c2d22531e1a6
Albert Ferras <albert.ferras@verse.me>
2022-12-20 23:48:22 +0100

    simplify

--- d20.py
+++ d20.py
@@ -24,38 +24,21 @@ class Number:
     position: int
 
 
-def new_pos(p, n):
-    return p % (n - 1)
-
-
 def solve(m=1, mix_times=1):
     values = read()
     values = [Number(x * m, i) for i, x in enumerate(values)]
-    size = len(values)
+    n = len(values)
 
     mix_order = [n for n in values]
     for round_n in range(mix_times):
-        print("ROUND", round_n)
         for node in mix_order:
-            i = node.position
-            new_i = (node.x + i) % (size - 1)
-            if i == new_i:
-                continue
-            for n in values[i+1:]:
-                n.position -= 1
-            values.pop(i)
-            values.insert(new_i, node)
-            node.position = new_i
-            for n in values[new_i+1:]:
-                n.position += 1
-
+            i = values.index(node)
+            if node.x != 0:
+                values.pop(i)
+                values.insert((node.x + i) % (n - 1), node)
     values = [n.x for n in values]
     z = values.index(0)
-    part1_sum = 0
-    for i in [1000, 2000, 3000]:
-        val = values[new_pos((i+z) % size, size)]
-        part1_sum += val
-    print("SUM=", part1_sum)
+    print(sum(values[(i+z) % n] for i in (1000, 2000, 3000)))
 
 
 solve(m=1)
